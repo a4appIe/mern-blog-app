e305c2a081c44b8329be12652ae2e6ce551ed3d9
Anthony <nitinbhagatxyz2@gmail.com>
2024-11-05 12:35:26 +0530

    changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <Route path="/signin" element={<Authform type={"signin"} />}></Route>
           <Route path="/add-blog" element={<AddBlog />}></Route>
           <Route path="/blog/:id" element={<BlogPage />}></Route>
+          <Route path="/edit/:id" element={<AddBlog />}></Route>
         </Route>
       </Routes>
     </div>
--- frontend/src/components/BlogPage.jsx
+++ frontend/src/components/BlogPage.jsx
@@ -9,8 +9,10 @@ const BlogPage = () => {
   const [blogData, setBlogData] = useState(null);
   const fetchBlogById = async () => {
     try {
-      const res = await axios.get(`${import.meta.env.VITE_BACKEND_URL}/blogs/${id}`);
-      setBlogData(res.data.blog)
+      const res = await axios.get(
+        `${import.meta.env.VITE_BACKEND_URL}/blogs/${id}`
+      );
+      setBlogData(res.data.blog);
       console.log(res.data.blog);
     } catch (error) {
       toast.error(error);
@@ -19,7 +21,14 @@ const BlogPage = () => {
   useEffect(() => {
     fetchBlogById();
   }, []);
-  return <div>{blogData ? <div> Blog Data </div> : <h1>Loading...</h1>}</div>;
+  return <div>{blogData ? <div className="flex items-center justify-center w-screen mt-10">
+    <div className="w-2/3 h-full">
+      <h1 className="text-4xl font-bold text-gray-700">{blogData.title}</h1>
+      <p className="text-gray-600">{blogData.creator.name}</p>
+      <img src={blogData.image} alt="" className="w-full h-[600px] object-cover mt-5" />
+      <button className="bg-green-300 px-5 py-2 rounded hover:bg-green-400">Edit</button>
+    </div>
+  </div> : <h1>Loading...</h1>}</div>;
 };
 
 export default BlogPage;
